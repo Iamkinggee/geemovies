6c747c14f8d229b6871c2b352922241a52914ef8
Iamkinggee <iamgodsentt@gmail.com>
2024-07-18 12:54:17 +0100

    added rows

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Main from '../Components/Main'
+import Row from '../Components/Row'
+import requests from '../requests'
 
 function Home() {
   return (
     <>
         <Main/>
+        <Row rowID='1'  title='Trending' fetchURL={requests. requestsPopular}/>
+        <Row rowID='2' title='UpComing' fetchURL={requests. requestsPopular}/>
+        <Row rowID='3'   title='Popular' fetchURL={requests. requestsPopular}/>
+        <Row  rowID='4'  title='Horror' fetchURL={requests. requestsPopular}/>
     </>
   )
 }
