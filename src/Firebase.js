9e0a10313d24953b5bb44fabb17e7697d2d65e44
Iamkinggee <iamgodsentt@gmail.com>
2024-07-18 15:35:42 +0100

    Integrating Firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAskrnV37nUJbI1FLxIsJEKKgEVVUCK1ww",
+  authDomain: "geemovies-d6f6b.firebaseapp.com",
+  projectId: "geemovies-d6f6b",
+  storageBucket: "geemovies-d6f6b.appspot.com",
+  messagingSenderId: "632577228698",
+  appId: "1:632577228698:web:5bec16006dd1a78b544423"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
